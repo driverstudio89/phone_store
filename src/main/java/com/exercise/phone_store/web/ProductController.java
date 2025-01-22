6f4fa3ad7a402032fe23f51c2424a8c9a641683a
Plamen Petrov <133807832+driverstudio89@users.noreply.github.com>
2025-01-22 20:40:05 +0200

    Feature/exeption handling 400 (#1)
    * implemented exception handler - bad request
    
    * implemented exception handler - bad request + cleaning imports
    
    * implemented exception handler - bad request + cleaning imports

--- src/main/java/com/exercise/phone_store/web/ProductController.java
+++ src/main/java/com/exercise/phone_store/web/ProductController.java
@@ -1,17 +1,15 @@
 package com.exercise.phone_store.web;
 
-import com.exercise.phone_store.model.Product;
 import com.exercise.phone_store.service.ProductService;
 import com.exercise.phone_store.service.util.Util;
 import com.exercise.phone_store.web.dto.AddProductDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
