e7c0c3a8fa23c62eba1a443f3c26901533e10eb9
driver <driver.studio.89@gmail.com>
2025-01-31 18:53:47 +0200

    added missing repository annotations

--- src/main/java/com/exercise/phone_store/data/CategoryRepository.java
+++ src/main/java/com/exercise/phone_store/data/CategoryRepository.java
@@ -5,9 +5,11 @@ import com.exercise.phone_store.model.enums.CategoryType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByCategory(CategoryType category);
--- src/main/java/com/exercise/phone_store/data/ProductRepository.java
+++ src/main/java/com/exercise/phone_store/data/ProductRepository.java
@@ -2,12 +2,14 @@ package com.exercise.phone_store.data;
 
 import com.exercise.phone_store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     Optional<Product> findByModel(String model);
